9c4896e56bc78a5f81bc8d5cc968b8a73fb3257e
giyun-cloud <nangiyoon@gmail.com>
2021-08-17 08:56:07 +0900

    callback

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-function common(params) {
+function common() {
   console.log(1);
   setTimeout(function () {
     console.log(2);
   }, 1000);
   console.log(3);
 }
-common();
+// common();
+
+function callback() {
+  console.log(1);
+  function call(cd) {
+    setTimeout(function () {
+      console.log(2);
+      cd();
+    }, 1000);
+  }
+  function back() {
+    console.log(3);
+  }
+  call(back);
+}
+
+callback();
