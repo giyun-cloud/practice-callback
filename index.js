1a0a54f4300f3da5651b468dc96390411a8eb956
giyun-cloud <nangiyoon@gmail.com>
2021-08-23 08:55:54 +0900

    generator function

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-function saleRate(rate) {
-  return function price(price) {
-    return price - price * rate * 0.01;
+function addOne() {
+  let result = 0;
+  return function add(add = 0) {
+    if (add) result += add;
+    else result++;
+    console.log(result);
+    return result;
   };
 }
 
-const summerRate = saleRate(20);
-const winterRate = saleRate(25);
+for (let i = 0; i < 5; i++) {
+  addOne()();
+}
 
-console.log(summerRate(10000), winterRate(10000));
+const result = addOne();
+
+for (let i = 0; i < 5; i++) {
+  result();
+}
+
+result(5);
+result();
+console.log("🟢🟢🟢🟢🟢🟢🟢🟢");
+function* generatorAddOne() {
+  let i = 1;
+  while (true) {
+    console.log(i);
+    const add = yield i;
+    if (add) i += add;
+    else i++;
+  }
+}
+
+const genResult = generatorAddOne();
+for (let i = 0; i < 5; i++) {
+  genResult.next();
+}
+genResult.next(5);
+genResult.next();
