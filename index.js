a53b8e58fc4219b53f315d8a648c0efc50f4db8f
giyun-cloud <nangiyoon@gmail.com>
2021-08-28 14:17:39 +0900

    for and array function

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const myIterable = {};
+const obj = [
+  { a: 1, b: "B", c: "CCC" },
+  { a: 2, b: "B", c: "CCC" },
+  { a: 3, b: "B", c: "CCC" },
+];
 
-myIterable[Symbol.iterator] = function* () {
-  let i = 1;
-  while (i <= 100) {
-    yield i++;
+const cart = [];
+
+for (const item of obj) {
+  const row = [];
+
+  for (const [, value] of Object.entries(item)) {
+    row.push(value);
   }
-};
 
-for (const n of myIterable) {
-  console.log(n);
+  cart.push(row);
 }
+
+console.log(cart);
+console.log(cart.join("==="));
+
+const result = obj.map((obj) => {
+  console.log("첫번째 map넘어온 obj", obj);
+  const obj2 = Object.entries(obj);
+  console.log("entries한 obj", obj2);
+  return obj2.map(([, value]) => String(value));
+});
+console.log(result);
+console.log(result.join("==="));
