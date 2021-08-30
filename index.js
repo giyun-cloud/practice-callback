58ed3d83dc63be94dce2c4b6b30d6a51a059cf79
giyun-cloud <nangiyoon@gmail.com>
2021-08-30 13:41:56 +0900

    deep copy obj

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
-const onepieceData = `Name,Role,Power
-Luffy,Captian,100
-Zoro,Subcaptian,95
-Sangi,Cooker,95
-Nami,Mate,50`;
-
-class MakeObj {
-  constructor(source, separater = ",") {
-    const rawData = source.split("\n");
-
-    this.headers = rawData[0].split(separater);
-    this.rows = rawData
-      .filter((_, index) => index > 0)
-      .map((row) => row.split(separater));
-  }
-
-  row = (index) =>
-    this.rows[index].map((row, index) => [this.headers[index], row]);
-
-  get length() {
-    return this.rows.length;
-  }
-
-  get columnLength() {
-    return this.headers.length;
-  }
-}
-
-class MakeObj2 extends MakeObj {
-  make = () => {
-    let ary = [];
-    for (let i = 0; i < this.rows.length; i++) {
-      ary.push(this.row(i).reduce(
-        (a, [key, value]) => ({
-          ...a,
-          [key]: value,
-        }),
-        {},
-      ));
+function deepCopyObj(obj) {
+  const copyObj = {};
+  for (const key in obj) {
+    if (typeof obj[key] == "object") {
+      copyObj[key] = deepCopyObj(obj[key]);
+    } else {
+      copyObj[key] = obj[key];
     }
-    return ary
-  };
+  }
+  return copyObj;
 }
 
-const data = new MakeObj2(onepieceData);
-console.log(data.make());
+const obj1 = {
+  a: {
+    b: {
+      d: 1,
+    },
+    c: 1,
+  },
+  aa: {
+    bb: {
+      dd: {
+        ff: 1,
+      },
+      gg: 5,
+    },
+  },
+};
+
+const obj2 = {
+  a: {
+    b: 1,
+  },
+};
+deepCopyObj(obj2);
